d9c26d533e0e7ae42befefff622096de2302166d
Kye-T <49996340+Kye-T@users.noreply.github.com>
2019-04-25 20:35:58 +0100

    Create RestApiService.kt

---
+++ game/plugins/src/main/kotlin/gg.rsmod.plugins/service/restapi/RestApiService.kt
@@ -0,0 +1,35 @@
+package gg.rsmod.plugins.service.restapi
+
+import gg.rsmod.game.Server
+import gg.rsmod.game.model.World
+import gg.rsmod.game.service.Service
+import gg.rsmod.plugins.service.restapi.routes.CorsRoute
+import gg.rsmod.plugins.service.restapi.routes.RestApiRoutes
+import gg.rsmod.util.ServerProperties
+import spark.Spark.stop
+
+/**
+ * A [RestApiService] implementation that enables an interactive
+ * web server to share active data.
+ *
+ * @author KyeT <okaydots@gmail.com>
+ */
+
+class RestApiService : Service {
+    override fun init(server: Server, world: World, serviceProperties: ServerProperties) {
+        CorsRoute(serviceProperties.getOrDefault("origin", "*"), serviceProperties.getOrDefault("methods", "GET, POST"), serviceProperties.getOrDefault("headers", "X-PINGOTHER, Content-Type"))
+        RestApiRoutes().init(world, serviceProperties.getOrDefault("auth", false))
+    }
+
+    override fun postLoad(server: Server, world: World) {
+    }
+
+    override fun bindNet(server: Server, world: World) {
+
+    }
+
+    override fun terminate(server: Server, world: World) {
+        stop()
+    }
+
+}
