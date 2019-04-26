686760db3a6c77ceaf28469896ea75a4086b7fc0
Kye Thomas <49996340+Kye-T@users.noreply.github.com>
2019-04-26 23:47:41 +0100

    V1.0.2

--- game/plugins/src/main/kotlin/gg.rsmod.plugins/service/restapi/RestApiService.kt
+++ game/plugins/src/main/kotlin/gg.rsmod.plugins/service/restapi/RestApiService.kt
@@ -3,22 +3,38 @@ package gg.rsmod.plugins.service.restapi
 import gg.rsmod.game.Server
 import gg.rsmod.game.model.World
 import gg.rsmod.game.service.Service
+import gg.rsmod.plugins.service.restapi.controllers.Controller
 import gg.rsmod.plugins.service.restapi.routes.CorsRoute
-import gg.rsmod.plugins.service.restapi.routes.RestApiRoutes
+import gg.rsmod.plugins.service.restapi.routes.Router
 import gg.rsmod.util.ServerProperties
+import mu.KLogger
 import spark.Spark.stop
 
 /**
- * A [RestApiService] implementation that enables an interactive
- * web server to share active data.
+ * A [RestApiService] to expose the Server cross-platform. Uses oAuth 2.0
+ * to allow for sensitive routes.
  *
  * @author KyeT <okaydots@gmail.com>
+ * @see "https://github.com/Kye-T/rsmod-restapi/README.md"
  */
 
 class RestApiService : Service {
     override fun init(server: Server, world: World, serviceProperties: ServerProperties) {
-        CorsRoute(serviceProperties.getOrDefault("origin", "*"), serviceProperties.getOrDefault("methods", "GET, POST"), serviceProperties.getOrDefault("headers", "X-PINGOTHER, Content-Type"))
-        RestApiRoutes().init(world, serviceProperties.getOrDefault("auth", false))
+        CorsRoute(serviceProperties.getOrDefault("origin", "*"), serviceProperties.getOrDefault("methods", "GET, POST, PUT, DELETE"), serviceProperties.getOrDefault("headers", "Content-Type"))
+
+        (serviceProperties.get<ArrayList<Any>>("routes") ?: ArrayList()).forEach { r ->
+            val values = r as LinkedHashMap<*, *>
+            val className = values["class"] as String
+            val clazz = Class.forName(className).asSubclass(Controller::class.java)!!
+            val route = clazz.newInstance()
+
+            val properties = hashMapOf<String, Any>()
+            values.filterKeys { it != "class" }.forEach { key, value ->
+                properties[key as String] = value
+            }
+
+            route.init(world, ServerProperties().loadMap(properties))
+        }
     }
 
     override fun postLoad(server: Server, world: World) {
@@ -31,5 +47,4 @@ class RestApiService : Service {
     override fun terminate(server: Server, world: World) {
         stop()
     }
-
 }
